0c2a5be64fab189534b8300bf345e0485404b356
Spinduck5384 <67084559+Spinduck5384@users.noreply.github.com>
2021-12-19 21:45:28 +0800

    Testing code for P.14

---
+++ 14.py
@@ -0,0 +1,22 @@
+"""
+If n is even, n/2
+If n is odd, 3n + 1
+"""
+
+longest_sequence = 0
+
+for num in range(1,1000001):
+    sequence = 1
+    n = num
+    while n > 1:
+        if n % 2 == 0:
+            n = n/2
+            sequence += 1
+        else:
+            n = 3*n + 1
+            sequence += 1
+    if sequence > longest_sequence:
+        longest_sequence = sequence
+    print(num, longest_sequence)
+
+
