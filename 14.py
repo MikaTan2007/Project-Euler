e6e6dfdae27fd1764df6b8f9a8d1ee6517348df1
Spinduck5384 <67084559+Spinduck5384@users.noreply.github.com>
2021-12-19 21:56:00 +0800

    Updated code

--- 14.py
+++ 14.py
@@ -4,9 +4,11 @@ If n is odd, 3n + 1
 """
 
 longest_sequence = 0
+seq_num = 0
 
 for num in range(1,1000001):
     sequence = 1
+    
     n = num
     while n > 1:
         if n % 2 == 0:
@@ -15,8 +17,8 @@ for num in range(1,1000001):
         else:
             n = 3*n + 1
             sequence += 1
+    
     if sequence > longest_sequence:
         longest_sequence = sequence
-    print(num, longest_sequence)
-
-
+        seq_num = num
+    print(seq_num, longest_sequence)
