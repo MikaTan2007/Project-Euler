3ced498c198142c9a355a6a93d5e2bd505511de5
Spinduck5384 <67084559+Spinduck5384@users.noreply.github.com>
2021-12-27 21:39:24 +0800

    Update PT test for better var names

--- 7/7pt.py
+++ 7/7pt.py
@@ -1,30 +1,35 @@
 """
-Uses Primality Test Optimization
+Python
+
+Primality Test Optimization
+
+Problem 7 Project Euler
+
+Mika
 """
 
 import time
 start_time = time.time()
+
 def isprime(num):
-    # 0, 1, even numbers greater than 2 are NOT PRIME
-    if num==1 or num==0 or (num % 2 == 0 and num > 2):
-        return "Not prime"
+    if num == 1 or num == 0:
+        return False
+    elif num % 2 == 0 and num > 2:
+        return False
     else:
-        # Not prime if divisable by another number less
-        # or equal to the square root of itself.
-        # n**(1/2) returns square root of n
-        for i in range(3, int(num**(1/2))+1, 2):
-            if num%i == 0:
+        for pot_num in range(3, int(num**(1/2))+1, 2):
+            if num % pot_num == 0:
                 return False
         return True
 
 primes = []
 
-count = 0
+num = 0
 
 while len(primes) < 10001:
-    if isprime(count) == True:
-        primes.append(count)
-    count += 1
+    if isprime(num) == True:
+        primes.append(num)
+    num += 1
 
 print(primes[-1])
 print("Program run time: " + str(time.time() - start_time))
