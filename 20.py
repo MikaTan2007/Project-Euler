72e3d72b442f0d3e37ef19c0aa982536b1bd1afd
Spinduck5384 <67084559+Spinduck5384@users.noreply.github.com>
2022-04-28 14:02:51 +0800

    Finished Problem 20

---
+++ 20.py
@@ -0,0 +1,25 @@
+"""
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+"""
+
+n = 100
+
+multiple = 1
+
+while n > 0:
+    multiple *= n
+    n -= 1
+
+print(multiple)
+
+sum = 0
+
+for n in str(multiple):
+    sum += int(n)
+
+print(sum)
